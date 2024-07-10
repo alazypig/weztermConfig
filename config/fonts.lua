9594c913fa0bb88853c2359cbe4720071f9c02b4
Edward Wang <wang.huiyang@outlook.com>
2024-07-10 16:36:36 +0800

    feat: update line height

--- config/fonts.lua
+++ config/fonts.lua
@@ -7,6 +7,7 @@ local font_size = platform().is_mac and 16 or 12
 return {
    font = wezterm.font(font),
    font_size = font_size,
+   line_height = 1.2,
 
    --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
    freetype_load_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
